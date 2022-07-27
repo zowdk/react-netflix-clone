7456d2a8c56fc24dfc98a845de93b32d5fc42f23
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-07-27 16:06:33 +0200

    fix spacing between h2 and img at smaller screens

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -34,6 +34,12 @@ export const Item = styled.div`
 
 export const Container = styled.section`
   background-color: black;
+
+  @media (max-width: 1000px) {
+    ${Item}:last-of-type h2 {
+      margin-bottom: 50px;
+    }
+  }
 `;
 
 export const Title = styled.h1`
