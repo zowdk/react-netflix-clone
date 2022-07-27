0b0cc03fec0be199d331adc079af4238bf48daba
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-07-27 23:53:54 +0200

    update jumbotron font styling

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -26,7 +26,7 @@ export const Pane = styled.div`
 
 export const Item = styled.div`
   display: flex;
-  border-bottom: 8px solid #222;
+  border-bottom: 5px solid #222;
   padding: 50px 5%;
   color: white;
   overflow: hidden;
@@ -46,6 +46,7 @@ export const Title = styled.h1`
   font-size: 50px;
   line-height: 1.1;
   margin-bottom: 8px;
+  letter-spacing: 0.5px;
 
   @media (max-width: 600px) {
     font-size: 35px;
@@ -54,8 +55,10 @@ export const Title = styled.h1`
 
 export const SubTitle = styled.h2`
   font-size: 26px;
+  font-family: sans-serif;
   font-weight: normal;
-  line-height: normal;
+  line-height: 1.3;
+  max-width: 50ch;
 
   @media (max-width: 600px) {
     font-size: 18px;
