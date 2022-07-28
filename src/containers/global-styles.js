629abdf7949f5a1314619fe41941b493f8768486
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-07-28 09:56:21 +0200

    setup folders for footer component

---
+++ src/containers/global-styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyles } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle` 
+html, body {
+    font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #000;
+    color: #333;
+    font-size: 16px;
+    margin: 0;
+
+}
+`;
