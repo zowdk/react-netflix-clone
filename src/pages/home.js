831415e0fc1051e182895f2318f6c98404d211d2
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-01 14:58:53 +0200

    add header bg img + style opt button

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,21 @@ import { FaqsContainer } from "../containers/faqs";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
+import { OptForm } from "../components";
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <p>Hello</p>
+        <OptForm>
+          <OptForm.Input placeholder="Email Address" />
+          <OptForm.Button>Get Started</OptForm.Button>
+          {/* //todo: insert break between input and text */}
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+        </OptForm>
       </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
