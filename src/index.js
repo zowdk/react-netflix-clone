05968e1f6976d7058996b1fe2ce109a1ec3b5236
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-02 13:49:42 +0200

    setup example firebase config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import { App } from "./app";
+import { FirebaseContext } from "./context/firebase";
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectID: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
 
 ReactDOM.render(
   <>
