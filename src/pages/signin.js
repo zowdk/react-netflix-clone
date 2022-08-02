74b1eb8df5bbdaafa80f1f8a85108b26632fdc67
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-02 15:45:32 +0200

    build out form elements on signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,11 +4,45 @@ import { HeaderContainer } from "../containers/header";
 
 export default function Signin() {
   const [error, setError] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    //call in here to firebase to authenticate the user
+    //if there's an error, populate the error state
+  };
   return (
     <HeaderContainer>
       <Form>
         <Form.Title>Sign In</Form.Title>
         {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Input
+            placeholder="Email address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          ></Form.Input>
+          <Form.Input
+            type="password"
+            value={password}
+            autoComplete="off"
+            placeholder="Password"
+            onChange={({ target }) => setEmailAddress(target.value)}
+          ></Form.Input>
+          <Form.Submit disabled={false} type="submit">
+            Sign In
+          </Form.Submit>
+          <Form.Text>
+            {" "}
+            New to Netflix?<Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA.
+          </Form.TextSmall>
+        </Form.Base>
       </Form>
     </HeaderContainer>
   );
