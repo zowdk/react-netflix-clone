875371b3a61dbe2797818d0f3d9fd33620757311
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-02 14:21:36 +0200

    connect application to firebase

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { App } from "./app";
 import { FirebaseContext } from "./context/firebase";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectID: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCRs5HAzvrlahIBfm5rNVwqz06eUSCvTpo",
+  authDomain: "react-netflix-clone-zowdk.firebaseapp.com",
+  projectId: "react-netflix-clone-zowdk",
+  storageBucket: "react-netflix-clone-zowdk.appspot.com",
+  messagingSenderId: "50461734855",
+  appId: "1:50461734855:web:84848a85d3b9ca48a44be2",
 };
 
 const firebase = window.firebase.initializeApp(config);
