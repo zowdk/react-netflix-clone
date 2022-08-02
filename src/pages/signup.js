b4d1bb0b1ad5ec5c8c26e8b351ce24b272e7a11e
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-02 16:26:11 +0200

    setup signup page

---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { HeaderContainer } from "../containers/header";
+
+export default function Signup() {
+  return (
+    <HeaderContainer>
+      <p>Hello</p>
+    </HeaderContainer>
+  );
+}
