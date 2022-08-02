786ffa53fe846f7532f285e7683bd8b9a5789542
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-02 15:02:46 +0200

    build out form component for signin page

---
+++ src/components/form/styles/form.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { Link as ReachRouterLink } from "react-router-dom";
+
+export const Container = styled.section``;
+
+export const Error = styled.div``;
+
+export const Form = styled.form``;
+
+export const Title = styled.h1``;
+
+export const Text = styled.p``;
+
+export const TextSmall = styled.h2``;
+
+export const Link = styled(ReachRouterLink)``;
+
+export const Input = styled.input``;
+
+export const Submit = styled.button``;
+
+// Container, Error, Base, Title, Text, TextSmall, Link, Input, Submit
