76d561740de01a15ffcce735875a8f9fa930f1e2
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-02 16:56:34 +0200

    remove comments

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -12,9 +12,6 @@ export default function Signin() {
 
   const handleSignin = (event) => {
     event.preventDefault();
-
-    //call in here to firebase to authenticate the user
-    //if there's an error, populate the error state
   };
   return (
     <>
