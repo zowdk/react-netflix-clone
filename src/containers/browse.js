ece4523f186093fba918ef3fcc9a2db1dab0081e
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-14 17:30:15 +0200

    fix play button to sit closer to content

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -51,8 +51,8 @@ export function BrowseContainer() {
             he projects in a futile attempt to feel like he's part of the world
             around him.
           </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
-        <Header.PlayButton>Play</Header.PlayButton>
       </Header>
       <FooterContainer />
     </>
