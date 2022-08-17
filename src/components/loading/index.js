58e8c0fa0f30ca36a4fa1fba668c31308243641f
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-17 12:27:47 +0200

    pseudo code for loading spinner component

---
+++ src/components/loading/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Loading({ }) {
+    return (
+
+    )
+}
+
+//spinner styled component
+//lockbody styled component
+    //will be a child of the spinner
+
+//a new CC component that will be the release body
