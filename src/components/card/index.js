b3f6ab04823a6b214a7fd1c6a1558395e79b2987
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-23 16:18:17 +0200

    build out card component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -7,8 +7,54 @@ export default function Card({ children, ...restProps }) {
   const [itemFeature, setItemFeature] = useState(false);
 
   return (
-    <featureContext.Provider value={{}}>
+    <featureContext.Provider
+      value={{ showFeature, setItemFeature, itemFeature, setItemFeature }}
+    >
       <Container {...restProps}>{children}</Container>
     </featureContext.Provider>
   );
 }
+
+Card.Group = function CardGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Card.Text = function CardText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }) {
+  return <Entities {...restProps}>{children}</Entities>;
+};
+
+Card.Meta = function CardMeta({ children, ...restProps }) {
+  return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Item = function CardItem({ item, children, ...restProps }) {
+  const { setShowFeature, setItemFeature } = useContext(featureContext);
+
+  return (
+    <Item
+      onClick={() => {
+        setItemFeature(item);
+        setShowFeature(true);
+      }}
+      {...restProps}
+    >
+      {children}
+    </Item>
+  );
+};
+
+Card.Image = function CardImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
