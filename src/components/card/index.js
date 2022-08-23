b98b8db7cf4e237ebd46e8a511bf3a4ab9c97a81
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-23 16:07:26 +0200

    create default state for card component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,7 +3,8 @@ import React, { useState, useContext, createContext } from "react";
 const featureContext = createContext();
 
 export default function Card({ children, ...restProps }) {
-    const [showFeature]
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState(false);
 
   return (
     <featureContext.Provider value={{}}>
