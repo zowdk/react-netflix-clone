8122b98d7a454f149efc987c3cf4cff707843ec0
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-08-23 16:41:50 +0200

    pull imports for card component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,5 +1,23 @@
 import React, { useState, useContext, createContext } from "react";
 
+import {
+  Container,
+  Group,
+  Title,
+  SubTitle,
+  Text,
+  Feature,
+  FeatureTitle,
+  FeatureText,
+  FeatureClose,
+  Maturity,
+  Content,
+  Meta,
+  Entities,
+  Item,
+  Image,
+} from "./styles/card";
+
 const featureContext = createContext();
 
 export default function Card({ children, ...restProps }) {
