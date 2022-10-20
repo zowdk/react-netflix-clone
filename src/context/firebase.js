3356079fb843c62bb5b261d04012a206e6ea2149
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-20 17:46:25 +0200

    install firebase for auto-completion

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -1,3 +1,4 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
 
+/** @type {React.Context<{firebase: import("firebase/app")}>} */
 export const FirebaseContext = createContext(null);
